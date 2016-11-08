dd984e533463d97cc9709364614f59c5981e6080
huangzhichang <729898301@qq.com>
2016-11-08 13:33:51 +0800

    pojo与xml互换的intellij Idea工程

---
+++ pojo2xml/src/com/keydak/jibx/Shipping.java
@@ -0,0 +1,9 @@
+package com.keydak.jibx;
+
+/**
+ * Created by admin on 2016/11/7.
+ */
+public enum Shipping {
+    STANDARD_MAIL, PRIORITY_MAIL, INTERNATIONAL_MAIL, DOMESTIC_EXPRESS,
+    INTERNATIONAL_EXPRESS
+}
